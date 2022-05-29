7accbb483fe8dfe620d5a5a4aeb2e638168098cb
MarkyMan4 <markus.hutnik@gmail.com>
2022-05-29 14:03:55 -0500

    insert statement

--- garden/models.py
+++ garden/models.py
@@ -9,11 +9,14 @@ class Plant(models.Model):
     name = models.CharField(null=False, max_length=200)
     inches_between_plants = models.IntegerField(null=False)
     inches_between_rows = models.IntegerField(null=False)
+    depth_inches = models.DecimalField(null=False, max_digits=3, decimal_places=2)
     days_to_germinate = models.IntegerField(null=False)
-    notes = models.CharField(null=False, max_length=1000)
+    days_to_harvest = models.IntegerField(null=False)
+    notes = models.CharField(null=True, max_length=1000)
 
 # plants that have been put into a garden
 class GardenPlant(models.Model):
     garden = models.ForeignKey(Garden, on_delete=models.CASCADE)
     plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    number_planted = models.IntegerField(null=False)
     date_planted = models.DateField(null=False)
---
+++ sql/plant_inserts.sql
@@ -0,0 +1,14 @@
+insert into garden_plant (
+    name,
+    inches_between_plants,
+    inches_between_rows,
+    days_to_germinate,
+    days_to_harvest,
+    depth_inches,
+    notes
+) values 
+('green bean', 3, 18, 14, 50, 1, 'Put stakes in the ground next to the plant to make it grow upwards.'),
+('spaghetti squash', 36, 36, 10, 100, 1, null),
+('carrot', 3, 36, 10, 75, 1, 'When planting, put four seeds in holes one inch apart. Once plants are two inches tall, thin them to three inches apart.'),
+('chamomile', 8, 18, 14, 60, 0.25, null),
+('cherry tomato', 24, 24, 10, 60, 0.25, null);
