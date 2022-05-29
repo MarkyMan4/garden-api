a172b9403da4c4fc73283a9c06bd7e7795ab6b30
MarkyMan4 <markus.hutnik@gmail.com>
2022-05-29 12:53:13 -0500

    initial commit

---
+++ garden/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Garden(models.Model):
+    name = models.CharField(null=False, max_length=500)
+    square_feet = models.DecimalField(null=False, max_digits=12, decimal_places=2)
+
+# information about plants
+class Plant(models.Model):
+    name = models.CharField(null=False, max_length=200)
+    inches_between_plants = models.IntegerField(null=False)
+    inches_between_rows = models.IntegerField(null=False)
+    days_to_germinate = models.IntegerField(null=False)
+    notes = models.CharField(null=False, max_length=1000)
+
+# plants that have been put into a garden
+class GardenPlant(models.Model):
+    garden = models.ForeignKey(Garden, on_delete=models.CASCADE)
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    date_planted = models.DateField(null=False)
